aea2d489cbc49a398f1cb72036676c43ccd37c67
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-09-20 14:40:26 +0200

    Books as String

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    RouterOutlet,
+    DashboardComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'book-rating';
+  title = 'Book Rating ⭐️';
 }
