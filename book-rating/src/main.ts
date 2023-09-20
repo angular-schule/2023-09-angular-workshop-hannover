be5bacd138b6c9b6005a1269deb3821d9a4eb502
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-09-20 11:13:27 +0200

    "useDefineForClassFields": true

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -4,3 +4,21 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+console.log('Hello ngWorld! 🤩')
+
+
+class Test {
+  blubb: number;
+
+
+  constructor(private zahl: number) {
+    console.log('Hallo', this.zahl);
+
+    // useDefineForClassFields
+    this.blubb = this.zahl + 1
+    console.log(this.blubb);
+  }
+}
+
+const test = new Test(42)
