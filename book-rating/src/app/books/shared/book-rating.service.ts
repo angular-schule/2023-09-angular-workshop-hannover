98c1200020924322cb15d242a0657bc1e472c13d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-09-22 11:45:40 +0200

    TDD ✅

--- book-rating/src/app/books/shared/book-rating.service.ts
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -6,11 +6,28 @@ import { Book } from './book';
 })
 export class BookRatingService {
 
+  readonly minRating = 1;
+  readonly maxRating = 5;
+
+  // optimale performance bei OnPush
   rateUp(book: Book): Book {
-    return book; // TODO: implement!
+    if (book.rating >= this.maxRating) {
+      return book;
+    }
+
+    return {
+      ...book,
+      rating: book.rating + 1
+    }
   }
 
+
   rateDown(book: Book): Book {
-    return book; // TODO
+    const rating =  Math.max(this.minRating, book.rating - 1);
+
+    return {
+      ...book,
+      rating
+    };
   }
 }
