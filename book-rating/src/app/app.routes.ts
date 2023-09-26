9dbb20dc5c98d77754bf33bda77f946260967fee
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-09-26 16:33:31 +0200

    Revert "loadChildren with export default BOOK_ROUTES"
    This reverts commit 32851f4b486f71186c653b4a74eb36e9d86b8d33.

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './books/dashboard/dashboard.component';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'books' },
-  { path: 'books', loadChildren: () => import('./books/book.routes') }
+  { path: 'books', component: DashboardComponent },
+  { path: 'books/:isbn', component: BookDetailsComponent }
   // { path: '**', component: NotFoundComponent }
 ];
