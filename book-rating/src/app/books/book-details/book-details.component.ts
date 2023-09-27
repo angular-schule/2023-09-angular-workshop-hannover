508fc6c9dec6c028a8de4e96d7bc10077b0c3289
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-09-27 15:10:59 +0200

    rxjs: final example

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
-import { BookStoreService } from '../shared/book-store.service';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { concatMap, map, mergeMap } from 'rxjs';
+import { catchError, map, of, retry, switchMap } from 'rxjs';
+
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [CommonModule, AsyncPipe, JsonPipe, RouterLink],
+  imports: [AsyncPipe, JsonPipe, RouterLink, NgIf],
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
@@ -18,7 +20,16 @@ export class BookDetailsComponent {
 
   book$ = inject(ActivatedRoute).paramMap.pipe(
     map(paramMap => paramMap.get('isbn')!),
-    concatMap(isbn => this.bs.getSingleBook(isbn))
+    switchMap(isbn => this.bs.getSingleBook(isbn).pipe(
+      retry({
+        count: 3,
+        delay: 1000
+      }),
+      catchError((e: HttpErrorResponse) => of({
+        title: 'FEHLER',
+        description: e.message
+      }))
+    ))
   );
 }
 
