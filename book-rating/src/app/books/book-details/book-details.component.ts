89e6dc3074a99563f718e8024a70c1f1175f8b24
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-09-27 15:13:56 +0200

    rxjs: final die 2.

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -3,7 +3,7 @@ import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { catchError, map, of, retry, switchMap } from 'rxjs';
+import { EMPTY, catchError, map, of, retry, switchMap } from 'rxjs';
 
 import { BookStoreService } from '../shared/book-store.service';
 
@@ -25,10 +25,11 @@ export class BookDetailsComponent {
         count: 3,
         delay: 1000
       }),
-      catchError((e: HttpErrorResponse) => of({
-        title: 'FEHLER',
-        description: e.message
-      }))
+      // catchError((e: HttpErrorResponse) => of({
+      //   title: 'FEHLER',
+      //   description: e.message
+      // }))
+      catchError((e: HttpErrorResponse) => EMPTY)
     ))
   );
 }
